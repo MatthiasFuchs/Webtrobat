c7ee1db7887f237b4e6080f82c46a5b1bd9d92a0
MatthiasFuchs <mat69@gmx.net>
2013-06-12 14:41:47 +0200

    [60][Testing] (MF) Test html for putting and getting projects into the indexed db

--- Webtrobat/js/Database.js
+++ Webtrobat/js/Database.js
@@ -2,17 +2,16 @@
 // IndexedDB
 var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB;
 IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction;
-dbVersion = 11;
-dbRecent = "webtrobat_recenta";
+dbVersion = 14;
+dbRecent = "recent";
+dbProjects = "projects";
  
 // Create/open database
 var request = indexedDB.open("elephantFiles", dbVersion);
 db = null;
 
-createObjectStore = function (dataBase)
+createObjectStoreRecent = function(dataBase)
 {
-    // Create an objectStore
-    console.log("Creating objectStore")
     try
     {
         dataBase.deleteObjectStore(dbRecent);
@@ -21,8 +20,32 @@ createObjectStore = function (dataBase)
     {
     }
     dataBase.createObjectStore(dbRecent);
+}
+
+createObjectStoreProjects = function(dataBase)
+{
+    try
+    {
+        dataBase.deleteObjectStore(dbProjects);
+    }
+    catch(e)
+    {
+    }
+    var objectStore = dataBase.createObjectStore(dbProjects, { keyPath: "id" });
+    objectStore.createIndex("name", "name", { unique: false });
+    objectStore.createIndex("script", "script", { unique: false });
+}
+
+createObjectStore = function(dataBase)
+{
+    // Create an objectStore
+    console.log("Creating objectStore");
+    
+    createObjectStoreRecent(dataBase);
+    createObjectStoreProjects(dataBase);
+
 };
- 
+
 setRecentProjectId = function(projectId)
 {
     console.log("Putting projectId in IndexedDB");
@@ -44,6 +67,52 @@ getRecentProjectId = function(callback)
         callback(event.target.result);
     }
 };
+
+
+putProject = function(project)
+{
+    console.log("Putting project in IndexedDB");
+    
+    // Open a transaction to the database
+    var transaction = db.transaction([dbProjects], "readwrite");
+    
+    // Put the blob into the dabase
+    var put = transaction.objectStore(dbProjects).put(project);
+};
+
+getProject = function(id, callback)
+{
+    var transaction = db.transaction([dbProjects]);
+    
+    var objectStore = transaction.objectStore(dbProjects);
+    objectStore.get(id).onsuccess = function (event)
+    {
+        var value = event.target.result;
+        callback({id: value.id, name: value.name, script: value.script});
+    }
+}
+
+getAllProjects = function(callback)
+{
+    var transaction = db.transaction([dbProjects]);
+    var objectStore = transaction.objectStore(dbProjects);
+    var result = [];
+    
+    objectStore.openCursor().onsuccess = function(event)
+    {
+        var cursor = event.target.result;
+        if (cursor)
+        {
+            var value = cursor.value;
+            result.push({id: value.id, name: value.name, script: value.script});
+            cursor.continue();
+        }
+        else
+        {
+            callback(result);
+        }
+    };
+}
 
 request.onerror = function(event)
 {
